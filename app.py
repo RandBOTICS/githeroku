c43b453857625579a02af3798263ce7cece726a7
RandBOTICS <94125172+RandBOTICS@users.noreply.github.com>
2022-04-13 14:23:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def predict_note_authentication(PlastificationTime, MaximumInjection, Cushion, M
 def main():
     st.title("Classificador Vipex")
     html_temp = """
-    <body style="background-color:blue
+    <body style="background-color:black>
     <div style="background-color:orange;padding:7px">
     <h2 style="color:white;text-align:center;">Processo de Injeção Sopro III </h2>
     </div>
